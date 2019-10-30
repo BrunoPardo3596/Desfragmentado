990ed7ee3d63f518d35c6628800f100be3b41ed2
Mitsuo-Yoshida <mit17yoshi@gmail.com>
2019-10-30 19:52:12 -0300

    Fix camera

---
+++ Camera2D.gd
@@ -0,0 +1,14 @@
+extends Camera2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Camera2D.ANCHOR_MODE_DRAG_CENTER
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
